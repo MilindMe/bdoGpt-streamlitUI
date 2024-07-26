2df40c388fd87fa50ffd266e614dc6858c2aebd9
unknown <m.meetarbhan@bdo.mu>
2024-07-26 13:48:45 +0400

    Extended context limit

--- Main.py
+++ Main.py
@@ -257,8 +257,8 @@ if prompt:
         txt += '   Generate a graph with graphviz in .dot \n'
 
 # Check : If length of text exceeds 8000, truncatenate and display with ... at the end
-    if len(txt) > 20000:
-        txt = txt[:20000] + '...'
+    if len(txt) > 100000:
+        txt = txt[:100000] + '...'
     if image or url != '':
         if url != '':
             img = Image.open(requests.get(url, stream=True).raw)
